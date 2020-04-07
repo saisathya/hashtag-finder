a1526a9b1108fa206306c03c820dad9cd9077d08
Jeffrey Zhang <zjeffreyl@gmail.com>
2020-04-07 15:00:34 -0700

    Implement multithreading for one hashtag search

--- src/main/java/com/hashtag_finder/services/InstagramHashtagCrawler.java
+++ src/main/java/com/hashtag_finder/services/InstagramHashtagCrawler.java
@@ -13,22 +13,22 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.regex.Pattern;
 
 @Service
 public class InstagramHashtagCrawler {
 
     final double percentangeThreshold = 0.10;
-
     public List<Hashtag> runGetHashtagsCrawler(String hashtag) throws InterruptedException {
         //Set up
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
         options.addArguments("--disable-features=VizDisplayCompositor");
-        //options.addArguments("headless");
+        options.addArguments("headless");
         WebDriver driver=new ChromeDriver(options);
         TimeUnit.SECONDS.sleep(1);
         driver.get("https://www.instagram.com/explore/tags/instagram");
@@ -131,74 +131,29 @@ public class InstagramHashtagCrawler {
         //open browser with desired URL
         int sum = 0;
         int count = 0;
+        List<Future<Integer>> futureLikes = new ArrayList<Future<Integer>>();
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(9);
         for(WebElement picture: topNine)
         {
-            int likes = getLikes(picture);
-            if(likes != -1)
+            Callable<Integer> callable = new PostCallable(picture);
+            Future<Integer> future = executor.submit(callable);
+            futureLikes.add(future);
+        }
+        for(Future<Integer> fut : futureLikes)
+        {
+            try {
+                int likes = fut.get();
+                if(likes != -1)
+                {
+                    sum += likes;
+                    count++;
+                }
+            } catch (InterruptedException | ExecutionException e)
             {
-                sum += likes;
-                count++;
+
             }
         }
         return sum/count;
-    }
-
-    public int getLikes(WebElement picture)
-    {
-        ChromeOptions options = new ChromeOptions();
-        //options.addArguments("headless");
-        options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
-        options.addArguments("--disable-features=VizDisplayCompositor");
-
-        WebDriver postDriver=new ChromeDriver(options);
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        postDriver.get(picture.findElement(By.xpath("a")).getAttribute("href"));
-        WebDriverWait wait = new WebDriverWait(postDriver, 10);
-        WebElement likesElement = null;
-        int counter = 0;
-        if(postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span")).isEmpty()) {
-            if(postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/span/span")).isEmpty())
-            {
-                return -1;
-            }
-            else
-            {
-                likesElement = postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span")).get(0);
-            }
-        }
-        else
-        {
-            likesElement = postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span")).get(0);
-        }
-//        while(likesElement == null)
-//        {
-//            try{
-//                Thread.sleep(100);
-//                likesElement = postDriver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span"));
-//            }
-//            catch(Exception e)
-//            {
-//                e.printStackTrace();
-//            }
-//            if(counter > 2000)
-//            {
-//                if(postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/span/span")).isEmpty())
-//                {
-//                    return -1;
-//                }
-//                likesElement = postDriver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/span/span"));
-//            }
-//            counter++;
-//        }
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span")));
-//        likesElement = postDriver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span"));
-        int retInteger = Integer.parseInt(likesElement.getText().replaceAll(",", ""));
-        postDriver.close();
-        return retInteger;
     }
 
     public List<Hashtag> getSortedHashtags(List<Hashtag> hashtags)
@@ -208,4 +163,55 @@ public class InstagramHashtagCrawler {
         return hashtags;
     }
 
+    class PostCallable implements Callable<Integer> {
+
+        private WebElement picture;
+
+        public PostCallable(WebElement picture)
+        {
+            this.picture = picture;
+        }
+
+
+        public int getLikes(WebElement picture)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("headless");
+            options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+            options.addArguments("--disable-features=VizDisplayCompositor");
+
+            WebDriver postDriver=new ChromeDriver(options);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            postDriver.get(picture.findElement(By.xpath("a")).getAttribute("href"));
+            WebDriverWait wait = new WebDriverWait(postDriver, 10);
+            WebElement likesElement = null;
+            int counter = 0;
+            if(postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span")).isEmpty()) {
+                if(postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/span/span")).isEmpty())
+                {
+                    return -1;
+                }
+                else
+                {
+                    likesElement = postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span")).get(0);
+                }
+            }
+            else
+            {
+                likesElement = postDriver.findElements(By.xpath("//*[@id=\"react-root\"]/section/main/div/div/article/div[2]/section[2]/div/div/button/span")).get(0);
+            }
+            int retInteger = Integer.parseInt(likesElement.getText().replaceAll(",", ""));
+            postDriver.close();
+            return retInteger;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            return getLikes(picture);
+        }
+    }
 }
